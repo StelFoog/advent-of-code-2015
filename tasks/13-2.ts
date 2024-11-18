c3b552cd380cad55d2f7500ba15f70510f1efa0c
Samuel Larsson <samuel@familjenlarsson.eu>
2024-11-18 09:34:29 +0100

    Day 13

---
+++ tasks/13-2.ts
@@ -0,0 +1,32 @@
+import elf from "elf-help";
+import { input } from "../src/inputManager";
+
+const feelings: Record<string, Record<string, number>> = {};
+const names = new Set<string>();
+
+for (const line of input.split("\n")) {
+	const splt = line.split(" ");
+	names.add(splt[0]);
+	if (!feelings[splt[0]]) feelings[splt[0]] = {};
+	feelings[splt[0]][splt[10].slice(0, -1)] = Number(splt[3]) * (splt[2] === "gain" ? 1 : -1);
+}
+
+const me = "Me";
+feelings[me] = {};
+for (const name of names) {
+	feelings[name][me] = 0;
+	feelings[me][name] = 0;
+}
+names.add(me);
+
+let max = Number.MIN_SAFE_INTEGER;
+for (const people of elf.permutations(Array.from(names))) {
+	let cur =
+		feelings[people[0]][people[people.length - 1]] + feelings[people[people.length - 1]][people[0]];
+	for (let i = 0; i < people.length - 1; i++) {
+		cur += feelings[people[i]][people[i + 1]] + feelings[people[i + 1]][people[i]];
+	}
+	if (cur > max) max = cur;
+}
+
+console.log(max);
